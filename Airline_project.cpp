b9cc932b96be85fa09bdcb03276ea52bfd6bfd6d
samruddha kumbhar <64959366+sam1231ui@users.noreply.github.com>
2022-11-08 19:56:55 +0530

    Update Airline_project.cpp

--- Airline_project.cpp
+++ Airline_project.cpp
@@ -1,23 +1,28 @@
 #include<bits/stdc++.h>
 #include<fstream>
-#include<iomanip>
+ 
 using namespace std;
 
 
+//Declaration of main function.
 void mainMenu();
-
+ 
+ 
+//class of management
 class management{
 	
 	public:
-		
+		//constructor which will directly call main menu.
 		management(){
 			mainMenu();
 		}
 };
 
+// class of Details to save or show details of user.
 class Details{
 	public:
 		
+		// data members that are required by Details class.
 		static string name, gender;
 		static int cId;
 		int phoneNo;
@@ -25,6 +30,7 @@ class Details{
 		string add;
 		char arr[100];
 		
+		// member function for information taking.
 		void information(){
 			cout<<"\nEnter the customer ID : ";
 			cin>>cId;
@@ -41,31 +47,41 @@ class Details{
 };
 
 
+// data members declaring global to use in furthur use.
 int Details::cId;
 string Details::name;
 string Details::gender;
 
+
+// class to register the data of user.
 class registration{
 	public:
-		static int choice;
+		
+		// data members of registrtion class.
+		static int choice; // this member is static declared to keep is same throughout the program.
 		int choice1;
 		int back;
-		static float charges;
+		static float charges; // this member is static declared to keep is same throughout the program.
 		
+		
+		// member function to show flight tickets.
 		void flights(){
 			
-			string flightN[] = {"Dubai", "Canada", "UK", "USA", "Germany"};
+			string flightN[] = {"Dubai", "Canada", "UK", "USA", "Germany"}; // array of flight tickets
 			
+			// showing each tickets to user.
 			for(int i=0; i<5; i++){
 				
-				cout<<i+1<<"Flight to "<<flightN[i]<<endl;
+				cout<<i+1<<" Flight to "<<flightN[i]<<endl;
 			}
 			
 			cout<<"Welcome to Airlines"<<endl;
 			
+			// taking input from user for selecting flight.
 			cout<<"Press the number for the flight: ";
 			cin>>choice;
 			
+			// switch case for the choice made.
 			switch(choice){
 				
 				case 1:{
@@ -81,9 +97,12 @@ class registration{
 					cout<<"1. DUB-433"<<endl;
 					cout<<"\n t06-01-2022 6:00PM 4hr Rs. 10000"<<endl;
 					
+					// inner input from user to select ticket for required flight
 					cout<<"Select the flight you want to book ";
 					cin>>choice1;
 					
+					
+					// condition to give output according to input.
 					if(choice1 == 1){
 						
 						charges = 14000;
@@ -110,6 +129,7 @@ class registration{
 						flights();
 					}
 					
+					// taking user back to main menu.
 					cout<<"Press any key to go to main menu"<<endl;
 					cin>>back;
 					
@@ -333,18 +353,24 @@ class registration{
 };
 
 
+
+// data members of registration to use in further code.
 float registration::charges;
 int registration::choice;
 
+// Derived class ticket from base classes (Multiple - inheritance)
 class ticket:public registration, Details{
 	
 	public:
 		
+		// member function to show bill of user
 		void Bill(){
 			
 			string destination = "";
-			ofstream outf("records.txt");
+			ofstream outf("records.txt"); // this is use of ofstream class to store data in disk memory.
 			{
+				
+				// data need to be showed on terminal.
 				outf<<"___________Airlines_________"<<endl;
 				outf<<"____________Tickets_________"<<endl;
 				outf<<"_____________________________"<<endl;
@@ -373,31 +399,39 @@ class ticket:public registration, Details{
 				outf<<"Destination: "<<destination<<endl;
 				outf<<"flight cost : "<<registration::charges<<endl;
 			}
+			// saving and closing file.
 			outf.close();
 			
 		}
 		
+		// member function to show all deatils in file.
 		void displayBill(){
 			
-			ifstream ifs("records.txt");
+			ifstream ifs("records.txt"); // using ifstream class to read the data from disk file.
 			{
+				// checking if file is avialable.
 				if(!ifs){
 					cout<<"File error !"<<endl;
 					
 				}
+				// to read till end of file.
 				while(!ifs.eof()){
 					
-					ifs.getline(arr,100);
-					cout<<arr<<endl;
+					ifs.getline(arr,100); // using getline function to get all data from file to show.
+					cout<<arr<<endl; // this is array we declared earlier in our details class and as it is derived class we can access that member.
 				}
 			}
+			// closing file.
 			ifs.close();
 		}
 };
 
 
+
+// Main menu class body as head we declared top of program.
 void mainMenu(){
 	
+	// data member of class
 	int lchoice;
 	int schoice;
 	int back;
@@ -406,102 +440,110 @@ void mainMenu(){
 	cout<<"\t______________Main Menu______________"<<endl;
 	
 	cout<<"\t________________________________________"<<endl;
-	cout<<"\t|\t\t\t\t\t\t|"<<endl;
+	cout<<"\t|\t\t\t\t\t\t"<<endl;
 	
-	cout<<"\t|\t Press 1 to add the customer details            \t"<<endl;
-	cout<<"\t|\t Press 2 for flight Registration            	\t"<<endl;
-	cout<<"\t|\t Press 3 to for tickets details            		\t"<<endl;
-	cout<<"\t|\t Press 4 to Exit                                \t"<<endl;
-	cout<<"\t|\t\t\t\t\t|"<<endl;
+	cout<<"\t|\t Press 1 To add the customer details.           \t"<<endl;
+	cout<<"\t|\t Press 2 For flight Registration.            	\t"<<endl;
+	cout<<"\t|\t Press 3 To for tickets details.           		\t"<<endl;
+	cout<<"\t|\t Press 4 To Exit.                                \t"<<endl;
+	cout<<"\t|\t\t\t\t\t"<<endl;
 	
-	
+	// taking chocie input from user.
 	cout<<"Enter your choice :  ";
-	cin>>lchoice;
+	cin >> lchoice;
 	
-	Details d;
-	registration r;
-	ticket t;
-	
-	switch(lchoice){
+	// checking if input is valid or not.
+	if(cin.fail())
+		cout<<" Please Press valid Integer"<<endl;
+			
+	else{
+	 
+		// objects declaration.
 		
-		
-		case 1:{
+		Details d; // Details class obj.
+		registration r; // registration class obj.
+		ticket t; // ticket class obj.
+		 
+		 
+		 // switch case to give output according to input.
+		switch(lchoice){
 			
-			cout<<"_______________Customers________________\n"<<endl;
 			
-			d.information();
-			
-			cout<<"Press 1 to go back to Main Menu";
-			cin>>back;
-			
-			if(back == 1)
-				mainMenu();
-			else
-				mainMenu();
+			case 1:{
 				
-			break;
-		}
-		
-		case 2:{
+				cout<<"_______________Customers________________\n"<<endl;
+				
+				d.information(); // member function call using dot operator.
+				
+				cout<<"Press 1 to go back to Main Menu";
+				cin>>back;
+				
+				if(back == 1)
+					mainMenu();
+				else
+					mainMenu();
+					
+				break;
+			}
 			
-			cout<<"_____________Book a flight ___________\n"<<endl;
+			case 2:{
+				
+				cout<<"_____________Book a flight ___________\n"<<endl;
+				
+				r.flights();// member function call using dot operator.
+				break;
+			}
 			
-			r.flights();
-			break;
-		}
-		
-		
-		case 3:{
 			
-			cout<<"__________Get YOUR Ticket_________\n"<<endl;
-			t.Bill();
-			
-			cout<<"Your ticket is ready, you can collect it \n"<<endl;
-			cout<<"Press 1 to display your ticket ";
-			
-			cin>>back;
-			
-			if(back == 1){
-				t.displayBill();
-				cout<<"Press any key to go back to Main menu";
+			case 3:{
+				
+				cout<<"__________Get YOUR Ticket_________\n"<<endl;
+				
+				t.Bill();// member function call using dot operator.
+				
+				cout<<"Your ticket is ready, you can collect it \n"<<endl;
+				cout<<"Press 1 to display your ticket ";
+				
 				cin>>back;
 				
 				if(back == 1){
-					mainMenu();
+					t.displayBill();// member function call using dot operator.
+					cout<<"Press any key to go back to Main menu";
+					cin>>back;
+					
+					if(back == 1){
+						mainMenu();
+					}else
+						mainMenu();
+					
 				}else
 					mainMenu();
 				
-			}else
+				break;
+			}
+			
+			
+			case 4:{
+				
+				cout<<"___________Thank - You___________\n"<<endl;
+				break;
+			}
+			
+			default:{
+				
+				cout<<"Invalid input, Please try again"<<endl;
 				mainMenu();
-			
-			break;
+				break;
+			}
+				
+				
 		}
-		
-		
-		case 4:{
-			
-			cout<<"___________Thank - You___________\n"<<endl;
-			break;
-		}
-		
-		default:{
-			
-			cout<<"Invalid input, Please try again"<<endl;
-			mainMenu();
-			break;
-		}
-			
-			
 	}
-	
-	
-	
-	
+ 
 }
 
 
-
+// Driver function of system.
 int main(){
 	management();
 }
-
